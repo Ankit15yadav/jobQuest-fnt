4dbfe274dff24ae8c46612022a1c7da0f7308fe3
Ankit15yadav <135987492+Ankit15yadav@users.noreply.github.com>
2024-08-08 10:30:31 +0530

    adding company slice

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import authReducer from "../slice/authSlice";
 import profileReducer from "../slice/profileSlice";
+import companyReducer from "../slice/CompanySlice";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     profile: profileReducer,
+    company: companyReducer,
 })
 
 export default rootReducer;
---
+++ src/slice/CompanySlice.jsx
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    company: null,
+    editCompany: null,
+}
+
+const companySlice = createSlice({
+    name: "company",
+    initialState,
+    reducers: {
+        setCompany: (state, action) => {
+            state.company = action.payload
+        },
+        setEditCompany: (state, action) => {
+            state.editCompany = action.payload
+        },
+        resetCompany: (state) => {
+            state.company = null
+            state.editCompany = null;
+        },
+    },
+})
+
+export const { setCompany, setEditCompany, resetCopmany } = companySlice.actions;
+
+export default companySlice.reducer;
